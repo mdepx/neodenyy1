dea83f86fadf097e3534f88afb2738bf8bbcc4e3
Ruslan Bukin <br@freebsd.org>
2023-12-10 20:08:47 +0000

    cleanup

--- src/gpio.c
+++ src/gpio.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * Copyright (c) 2023 Ruslan Bukin <br@bsdpad.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,7 @@
 
 #include "gpio.h"
 
-static const struct gpio_pin uart_pins[] = {
+static const struct gpio_pin neodenyy1_pins[] = {
 	{ PORT_A,  9, MODE_ALT, 7, FLOAT }, /* USART1_TX */
 	{ PORT_A, 10, MODE_ALT, 7, FLOAT }, /* USART1_RX */
 
@@ -39,13 +39,16 @@ static const struct gpio_pin uart_pins[] = {
 	{ PORT_E, 1, MODE_OUT, 0, FLOAT }, /* Air 2 */
 	{ PORT_E, 0, MODE_OUT, 0, FLOAT }, /* N */
 
-	/* Needle Set ? */
+	/* Needle Set */
 	{ PORT_B, 5, MODE_INP, 0, FLOAT }, /* NS */
-	/* At least one head is at Z ? */
+
+	/* At least one head is at Z. */
 	{ PORT_B, 4, MODE_INP, 0, FLOAT }, /* SZ */
-	/* Head 1 has component? */
+
+	/* Head 1 has component. */
 	{ PORT_B, 3, MODE_INP, 0, FLOAT }, /* S1 */
-	/* Head 2 has component? */
+
+	/* Head 2 has component. */
 	{ PORT_D, 4, MODE_INP, 0, FLOAT }, /* S2 */
 
 	{ PORT_A,  3, MODE_OUT, 0, FLOAT }, /* Vibrator */
@@ -60,88 +63,9 @@ static const struct gpio_pin uart_pins[] = {
 	{ -1, -1, -1, -1, -1 },
 };
 
-static const struct gpio_pin dram_pins[] = {
-	{ PORT_B,  5, MODE_ALT, 12, FLOAT }, /* SDCKE1 */
-	{ PORT_B,  6, MODE_ALT, 12, FLOAT }, /* SDNE1 */
-
-	{ PORT_C,  0, MODE_ALT, 12, FLOAT }, /* FMC_SDNWE */
-	{ PORT_D,  0, MODE_ALT, 12, FLOAT }, /* FMC_D2 */
-	{ PORT_D,  1, MODE_ALT, 12, FLOAT }, /* FMC_D3 */
-	{ PORT_D,  8, MODE_ALT, 12, FLOAT }, /* FMC_D13 */
-	{ PORT_D,  9, MODE_ALT, 12, FLOAT }, /* FMC_D14 */
-	{ PORT_D, 10, MODE_ALT, 12, FLOAT }, /* FMC_D15 */
-	{ PORT_D, 14, MODE_ALT, 12, FLOAT }, /* FMC_D0 */
-	{ PORT_D, 15, MODE_ALT, 12, FLOAT }, /* FMC_D1 */
-
-	{ PORT_E,  0, MODE_ALT, 12, FLOAT }, /* FMC_NBL0 */
-	{ PORT_E,  1, MODE_ALT, 12, FLOAT }, /* FMC_NBL1 */
-	{ PORT_E,  7, MODE_ALT, 12, FLOAT }, /* FMC_D4 */
-	{ PORT_E,  8, MODE_ALT, 12, FLOAT }, /* FMC_D5 */
-	{ PORT_E,  9, MODE_ALT, 12, FLOAT }, /* FMC_D6 */
-	{ PORT_E, 10, MODE_ALT, 12, FLOAT }, /* FMC_D7 */
-	{ PORT_E, 11, MODE_ALT, 12, FLOAT }, /* FMC_D8 */
-	{ PORT_E, 12, MODE_ALT, 12, FLOAT }, /* FMC_D9 */
-	{ PORT_E, 13, MODE_ALT, 12, FLOAT }, /* FMC_D10 */
-	{ PORT_E, 14, MODE_ALT, 12, FLOAT }, /* FMC_D11 */
-	{ PORT_E, 15, MODE_ALT, 12, FLOAT }, /* FMC_D12 */
-
-	{ PORT_F,  0, MODE_ALT, 12, FLOAT }, /* A0 */
-	{ PORT_F,  1, MODE_ALT, 12, FLOAT }, /* A1 */
-	{ PORT_F,  2, MODE_ALT, 12, FLOAT }, /* A2 */
-	{ PORT_F,  3, MODE_ALT, 12, FLOAT }, /* A3 */
-	{ PORT_F,  4, MODE_ALT, 12, FLOAT }, /* A4 */
-	{ PORT_F,  5, MODE_ALT, 12, FLOAT }, /* A5 */
-	{ PORT_F, 11, MODE_ALT, 12, FLOAT }, /* FMC_SDNRAS */
-	{ PORT_F, 12, MODE_ALT, 12, FLOAT }, /* A6 */
-	{ PORT_F, 13, MODE_ALT, 12, FLOAT }, /* A7 */
-	{ PORT_F, 14, MODE_ALT, 12, FLOAT }, /* A8 */
-	{ PORT_F, 15, MODE_ALT, 12, FLOAT }, /* A9 */
-
-	{ PORT_G,  0, MODE_ALT, 12, FLOAT }, /* A10 */
-	{ PORT_G,  1, MODE_ALT, 12, FLOAT }, /* A11 */
-	{ PORT_G,  4, MODE_ALT, 12, FLOAT }, /* A14 / BA0 */
-	{ PORT_G,  5, MODE_ALT, 12, FLOAT }, /* A15 / BA1 */
-	{ PORT_G,  8, MODE_ALT, 12, FLOAT }, /* SDCLK */
-	{ PORT_G, 15, MODE_ALT, 12, FLOAT }, /* SDNCAS */
-
-	{ PORT_H,  2, MODE_ALT, 12, FLOAT }, /* SDCKE0 */
-	{ PORT_H,  3, MODE_ALT, 12, FLOAT }, /* SDNE0 */
-	{ PORT_H,  8, MODE_ALT, 12, FLOAT }, /* D16 */
-	{ PORT_H,  9, MODE_ALT, 12, FLOAT }, /* D17 */
-	{ PORT_H, 10, MODE_ALT, 12, FLOAT }, /* D18 */
-	{ PORT_H, 11, MODE_ALT, 12, FLOAT }, /* D19 */
-	{ PORT_H, 12, MODE_ALT, 12, FLOAT }, /* D20 */
-	{ PORT_H, 13, MODE_ALT, 12, FLOAT }, /* D21 */
-	{ PORT_H, 14, MODE_ALT, 12, FLOAT }, /* D22 */
-	{ PORT_H, 15, MODE_ALT, 12, FLOAT }, /* D23 */
-
-	{ PORT_I,  0, MODE_ALT, 12, FLOAT }, /* D24 */
-	{ PORT_I,  1, MODE_ALT, 12, FLOAT }, /* D25 */
-	{ PORT_I,  2, MODE_ALT, 12, FLOAT }, /* D26 */
-	{ PORT_I,  3, MODE_ALT, 12, FLOAT }, /* D27 */
-	{ PORT_I,  4, MODE_ALT, 12, FLOAT }, /* NBL2 */
-	{ PORT_I,  5, MODE_ALT, 12, FLOAT }, /* NBL3 */
-	{ PORT_I,  6, MODE_ALT, 12, FLOAT }, /* D28 */
-	{ PORT_I,  7, MODE_ALT, 12, FLOAT }, /* D29 */
-	{ PORT_I,  9, MODE_ALT, 12, FLOAT }, /* D30 */
-	{ PORT_I, 10, MODE_ALT, 12, FLOAT }, /* D31 */
-	{ -1, -1, -1, -1, -1 },
-};
-
-static const struct gpio_pin dsi_pins[] = {
-	{ PORT_A, 3, MODE_OUT, 0, FLOAT }, /* LCD_BL_CTRL */
-	{ PORT_H, 7, MODE_OUT, 0, FLOAT }, /* LCD_RESET */
-	{ PORT_J, 2, MODE_ALT, 13, FLOAT }, /* DSI_TE */
-	{ -1, -1, -1, -1, -1 },
-};
-
 void
 gpio_config(struct stm32f4_gpio_softc *sc)
 {
 
-	pin_configure(sc, uart_pins);
-	return;
-
-	pin_configure(sc, dram_pins);
-	pin_configure(sc, dsi_pins);
+	pin_configure(sc, neodenyy1_pins);
 }
